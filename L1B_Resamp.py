ad3e24619922f28072301dbd17a128c090249801
Harold Garbeil <hg@Harolds-MacBook-Pro.local>
2023-01-31 07:14:46 -1000

    added resamp file

---
+++ L1B_Resamp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 07:11:44 2023
+
+@author: hg
+"""
+from MOD021 import *
+from MOD03 import *
+
+
+import numpy as np 
+
+class L1B_Resamp :
+    ThermFile=None 
+    GeoFile=None
